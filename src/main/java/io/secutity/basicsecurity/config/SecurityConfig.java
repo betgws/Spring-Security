96bc54a481185450ca02d389478a0493771ff081
dayeong <dayeongyun184@gmail.com>
2024-03-06 17:22:57 +0900

    securityConfig 초기 작성

---
+++ src/main/java/io/secutity/basicsecurity/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package io.secutity.basicsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();//어떤 요청에도 인증을 받도록
+
+        http
+                .formLogin();
+    }
+
+}
