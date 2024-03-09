3a7c1e4815ee7d50fbd6093a4086e9d678803bd1
dayeong <dayeongyun184@gmail.com>
2024-03-09 11:05:47 +0900

    getMapping 추가

--- src/main/java/io/secutity/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/secutity/basicsecurity/config/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @Configuration
@@ -17,7 +26,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();//어떤 요청에도 인증을 받도록
 
         http
-                .formLogin();
+                .formLogin()
+                //.loginPage("/loginPage") //로그인페이지 설정
+                .defaultSuccessUrl("/") // 인증에 성공했을 때 갈 url
+                .failureUrl("/login") // 로그인 실패 시 다시 로그인 페이지로 이동
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication"+ authentication.getName()); // 인증의 성공한 사용자 이름 뽑기
+                        response.sendRedirect("/");
+                    }
+                }) //로그인에 성공했을 때 successHandler 호출
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println("exception" + exception.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                })
+                .permitAll(); //.loginPage("/loginPage") 로그인 하는 페이지는 인증을 받지 않아도 들어갈 수 있어야됨
     }
 
 }
